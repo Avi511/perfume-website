163159460016dacce9003d4e4c823e892aa30c17
Avishka Medagamagodage <aimedagamagodage2003@gmail.com>
2026-03-29 01:19:10 +0530

    feat: add order routes with CRUD operations and authentication middleware

--- Backend/src/routes/orderRoutes.js
+++ Backend/src/routes/orderRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { createOrder, getOrder, getOrderById, updateOrderStatus, deleteOrder } from "../controllers/orderController.js";
+import { authMiddleware as protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/create", createOrder);
-router.get("/", getOrder);
-router.get("/:id", getOrderById);
-router.put("/:id", updateOrderStatus);
-router.delete("/:id", deleteOrder);
+router.post("/", protect, createOrder);
+router.get("/", protect, getOrder);
+router.get("/:id", protect, getOrderById);
+router.put("/:id", protect, updateOrderStatus);
+router.delete("/:id", protect, deleteOrder);
 
 export default router;
