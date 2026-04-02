5689302ee61fca85710f3c8dc66f7180456a8c72
Avishka Medagamagodage <aimedagamagodage2003@gmail.com>
2026-04-02 14:32:26 +0530

    feat: initialize frontend project structure with axios service and backend express application setup

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,7 +14,12 @@ import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
